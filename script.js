0c7ca7ea082e7f0e673da8ebf8e7b1e00f8ef755
bamjam101 <herejamil123@gmail.com>
2022-01-24 20:39:34 +0530

    Page made responsive using raw CSS.

--- script.js
+++ script.js
@@ -5,16 +5,21 @@ function Authenticate() {
     const Tag = document.querySelector("#username").value;
     const Key = document.querySelector("#key").value;
 
-    if (TAG===Tag && KEY===Key) {
+    if (TAG === Tag && KEY === Key) {
         setTimeout(() => {
             document.querySelector(".landingPage").style.display = "none";
         }, 1000);
 
         document.querySelector(".landingPage").style.transform = "translateY(-100%)";
 
-        document.querySelector("header").style.opacity = "100";
-        document.querySelector("main").style.opacity = "100";
-        document.querySelector("footer").style.opacity = "100";
+        document.querySelector("header").style.display = "block";
+        document.querySelector("main").style.display = "flex";
+        document.querySelector("footer").style.display = "block";
+        setTimeout(() => {
+            document.querySelector("header").style.opacity = "100";
+            document.querySelector("main").style.opacity = "100";
+            document.querySelector("footer").style.opacity = "100";
+        }, 500);
     } else {
         alert("The Key Is Incorrect!");
     }
@@ -24,28 +29,51 @@ let toggle = true;
 
 function liveToggle() {
 
-    const liveBox = document.querySelector(".live");
+    const navBox = document.querySelector("nav");
     const form = document.querySelector("#selection");
     const footer = document.querySelector("footer");
-    const extendToggle = document.querySelector(".extend");
+    const liveBox = document.querySelector(".live");
+    const main = document.querySelector("main");
 
     if (toggle == true) {
-        form.style.transform = "translateX(-25vh)";
-        footer.style.transform = "translateX(-25vh)";
+        setTimeout(() => {
+            main.style.display = "none";
+            footer.style.display = "none";
+            liveBox.style.display = "none";
+        }, 1000);
 
-        extendToggle.style.top = "90vh"
-        extendToggle.style.transform = "rotateZ(120deg)"
-        liveBox.style.height = "100vh";
-        liveBox.style.clipPath = "polygon(100% 0, 100% 100%, 0 100%, 0 0)";
+        liveBox.style.transform = "translateX(25vh)";
+        liveBox.style.opacity = "0";
+
+        navBox.style.height = "100vh";
+        navBox.style.width = "100vw"
+        main.style.opacity = "0";
+        footer.style.opacity = "0";
+
+        // liveBox.style.height = "100vh";
+        // liveBox.style.clipPath = "polygon(100% 0, 100% 100%, 0 100%, 0 0)";
+
+        navBox.style.clipPath = "polygon(100% 0, 100% 100%, 0 100%, 0 0)";
     } else {
-        form.style.transform = "none";
-        footer.style.transform = "none";
+        liveBox.style.display = "block";
+        navBox.style.height = "17.5vh";
 
-        extendToggle.style.top = "1rem"
-        extendToggle.style.transform = "rotateZ(0)"
+        main.style.display = "flex";
+        footer.style.display = "block";
 
-        liveBox.style.height = "20vh";
-        liveBox.style.clipPath = "polygon(100% 0, 100% 100%, 0 50%, 0 0)";
+        main.style.opacity = "100";
+        footer.style.opacity = "100";
+
+        liveBox.style.opacity = "80%";
+        setTimeout(() => {
+            liveBox.style.transform = "translateX(0)";
+        }, 1000);
+        navBox.style.width = "75%";
+
+
+        // liveBox.style.height = "17.5vh";
+        // liveBox.style.clipPath = "polygon(100% 0, 100% 100%, 0 50%, 0 0)";
+        navBox.style.clipPath = "polygon(100% 0, 100% 50%, 0 100%, 0 0)";
         toggle = true;
         return;
     }
